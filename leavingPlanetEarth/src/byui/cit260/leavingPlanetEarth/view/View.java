4fe5a9416927b9fe3f221609a3a28ee3e0306a80
OptimusPrime <OptimusPrime@10.0.1.4>
2016-03-27 22:21:19 -0600

    01/27/16 Leaving Planet Earth player class created 01/27/16 Leaving Planet Earth Start Location class created 01/27/16 Leaving Planet Earth Desert class created 01/27/16 Leaving Planet Earth NASA class created 01/27/16 Leaving Planet Earth Inventory class created 01/27/16 Leaving Planet Earth Parts class created 01/27/16 Leaving Planet Earth Tools class created 01/27/16 Leaving Planet Earth Food class created

--- leavingPlanetEarth/src/byui/cit260/leavingPlanetEarth/view/View.java
+++ leavingPlanetEarth/src/byui/cit260/leavingPlanetEarth/view/View.java
@@ -14,56 +14,132 @@ import leavingPlanetEarth.LeavingPlanetEarth;
  *
  * @author OptimusPrime
  */
-public abstract class View implements ViewInterface {
 
-    protected String displayMessage;
+public abstract class View implements ViewInterface {
+    
+    private String promptMessage;
+    private boolean input = true;
     
     protected final BufferedReader keyboard = LeavingPlanetEarth.getinFile();
     protected final PrintWriter console = LeavingPlanetEarth.getOutFile();
 
     public View() {
+        this.input = true;
+    }
+    
+    public View(boolean input, String message) {
+        this.input = input;
+        this.promptMessage = message;
     }
 
     public View(String message) {
-        this.displayMessage = message;
+        this.promptMessage = message;
+        this.input = true;
     }
 
+    public String getPromptMessage() {
+        return promptMessage;
+    }
+
+    public void setPromptMessage(String message) {
+        this.promptMessage = message;
+    }
+    
     @Override
     public void display() {
-        char selection = ' ';
-        do {
-
-            this.console.println();//display the main menu
-
-            String input = this.getInput(); // get the user's selection
-            selection = input.charAt(0); // get first character of string
-
-            this.doAction(selection); // do action based on selection
-
-        } while (selection != 'E'); // an slection is not "Exit"
-
-    }
-
-    @Override
-    public String getInput() {
-        boolean valid = false;
-        String value = null;
-        try{
-          
-        while (!valid) {
-
-            value=this.keyboard.readLine();
-            value = value.trim();
-
-            if (value.length() < 1) {
-                this.console.println("\n*** You must enter a value ***");
-                continue;
+        String selection= "";
+        boolean done = false;
+        
+        do { 
+            this.console.println(this.promptMessage); // display the prompt promptMessage
+            if (this.input) {
+                selection = this.getInput(); // get the user's selection
             }
-            break;
-        }
-        } catch (Exception e){
-            this.console.println("Error reading input:"+ e.getMessage());
-        }
-        return value;
+
+            done = this.doAction(selection); // do action based on selection        
+        } while (!done);
+
     }
+    
+    
+     @Override
+    public String getInput() {
+
+        boolean valid = false;
+        String selection = null;
+        try {
+            // while a valid name has not been retrieved
+            while (!valid) {
+
+                // get the value entered from the keyboard
+                selection = this.keyboard.readLine();
+                selection = selection.trim();
+
+                if (selection.length() < 1) { // blank value entered
+                    ErrorView.display(this.getClass().getName(), 
+                                      "You must enter a value.");
+                    continue;
+                }
+
+                break;
+            }
+        } catch (Exception e) {
+            ErrorView.display(this.getClass().getName(), 
+                              "Error reading input: " + e.getMessage());
+            return null;
+        }
+
+        return selection; // return the name        
+    }
+       
+    public void clearConsole() {
+        try {
+            final String os = System.getProperty("os.name");
+
+            if (os.contains("Windows")) {
+                Runtime.getRuntime().exec("cls");
+            } else {
+                Runtime.getRuntime().exec("clear");
+            }
+        } catch (final Exception e) {
+            ErrorView.display(this.getClass().getName(), 
+                              "Error clearing the console: " + e.getMessage());
+        }
+    }
+   
+    
+    public String getBlockedMessage(String message) {
+        String newMessage = "";
+        int noOfLines = (message.length() / 81) + 1;
+        String[] words = message.split(" ");
+        String line = "";
+        
+        for (String word : words) {
+            
+            if (line.length() + word.length() < 80) {
+                line += word.trim() + " ";
+            }
+            else {
+                newMessage += line + "\n";
+                line = word.trim() + " ";
+            }
+        }
+        newMessage += line;
+        
+        
+        return newMessage;
+        
+        
+    }
+
+   
+
+   
+   
+  
+
+    
+
+  
+    
 }
